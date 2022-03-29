8e303b5a4a9d507995a05f7ae1ba285a242ac5d1
Tesfay-Hagos <tesfay.hagos1421@gmail.com>
2022-03-30 04:22:42 +0500

    the paddle class is coded to create two separate paddles and make listen to Up, Down, W and s keys

--- ball.py
+++ ball.py
@@ -10,6 +10,7 @@ class Ball(Turtle):
         self.penup()
         self.x_move = 10
         self.y_move = 10
+        self.move_speed = 0.1
 
     def move(self):
         new_x = self.xcor() + self.x_move
@@ -21,8 +22,12 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.x_move *= -1
+        self.move_speed *= 0.8
 
     def re_fresh(self):
         self.goto(0, 0)
+        self.move_speed = 0.1
         self.bounce_x()
+
+
 
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
 import time
+from scoreboard import ScoreBoard
+score_board = ScoreBoard()
 screen = Screen()
 screen.tracer(0)
 screen.bgcolor("black")
@@ -13,23 +15,27 @@ r_paddle = Paddle((350, 0), "Up", "Down")
 l_paddle = Paddle((-350, 0), "w", "s")
 ball = Ball()
 game_is_on = True
+
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
+    # detect collision with the wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    # detect collision with r_paddle
+    # detect collision with paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
     # detect if the r_paddle misses the ball
     if ball.xcor() > 350:
         ball.re_fresh()
+        score_board.l_point()
     # detect if the l_paddle misses the ball
     if ball.xcor() < -350:
         ball.re_fresh()
+        score_board.r_point()
 
 
 
